0cb803a7aea71c381727b1382b05179e401042c6
Student <Student@TC8-MAC19.local>
2017-11-04 15:07:20 -0400

    first time

---
+++ PB&J.rb
@@ -0,0 +1,45 @@
+bread = ["white", "wheat", "rye"]
+choose_bread = gets
+slice = "one slice"
+pb = ["yes", "no"]
+choose_pb = gets
+jelly = ["yes", "no"]
+choose_jelly = gets
+jar_of_pb = "peanut buttery goodness"
+jar_of_jelly = "sweet delicious goo"
+knife = 65
+put_together = "sandwich complete"
+eat = sample "eat"
+
+define make_lunch
+  print "what kind of bread do you like? We Have"
+  print bread
+  choose_bread
+  print "you have choosen" + choose_bread
+  sleep 1
+  print "do you want peanut butter"
+  choose_pb
+  if choose_pb == "yes"
+    play slice
+    sleep 1
+    print jar_of_pb
+    sleep 1
+    print knife
+    sleep 1
+  end
+  print "do you want jelly"
+  choose_jelly
+  if choose_jelly == "yes"
+    print slice
+    sleep 1
+    print jar_of_jelly
+    sleep 1
+    print knife
+    sleep 1
+  end
+  print put_together
+  sleep 1
+  print eat
+  sleep 1
+end
+make_lunch
